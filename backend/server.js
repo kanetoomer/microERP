b4acd6615497b339d7c539a676fd26e8a7d7a750
Kane Toomer <kanetoomer@yahoo.com>
2025-02-20 19:48:02 -0800

    starting w/ the backend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./src/config/db");
+
+// Environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Initialize Express app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const authRoutes = require("./src/routes/authRoutes");
+// const dataRoutes = require("./src/routes/dataRoutes");
+// const reportsRoutes = require("./src/routes/reportsRoutes");
+
+// Use routes
+app.use("/api/auth", authRoutes);
+// app.use("/api/data", dataRoutes);
+// app.use("/api/reports", reportsRoutes);
+
+// Root Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,46 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ error: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Registration failed", details: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: "User not found!" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ error: "Incorrect password!" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({
+      token,
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (error) {
+    console.error("Login Error:", error);
+    res.status(500).json({ error: "Login Failed!", details: error.message });
+  }
+};
+
+module.exports = { register, login };
